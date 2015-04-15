bc18a074a9fd817a11c510f3b63cb5b61d7b2fb1
lang <bm2736892@gmail.com>
2015-04-15 16:37:29 +0800

    首页字体，样式调整

---
+++ css/font/build.js
@@ -0,0 +1,17 @@
+var Fontmin = require('fontmin');
+var htmlToText = require('html-to-text');
+var path = require('path');
+
+htmlToText.fromFile(path.join(__dirname, '../../index.html'), {}, function (err, text) {
+    new Fontmin()
+        .src('./noto-thin.ttf')
+        .use(Fontmin.glyph({ 
+            text: text
+        }))
+        .run(function (err, files) {
+            if (err) {
+                throw new Error(err);
+            }
+            require('fs').writeFileSync('./noto-thin.min.ttf', files[0]._contents);
+        });
+})
